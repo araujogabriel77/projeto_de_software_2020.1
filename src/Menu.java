2190c004331f99a2c0242af001f03c895aa122f7
araujogabriel77 <araujogabrielocn@gmail.com>
2021-03-17 08:26:15 -0300

    update

--- src/Menu.java
+++ src/Menu.java
@@ -3,17 +3,10 @@ import java.util.Scanner;
 public class Menu {
     public static void main(String[] args) {
         class OptionsMenu {
-            public String op_1 = "Adicionar um empregado";
-            public String op_2 = "Remover um empregado";
-            public String op_3 = "Lançar um cartão de ponto";
-            public String op_4 = "Lançar um resultado de venda";
-            public String op_5 = "Lançar uma taxa de serviço";
-            public String op_6 = "Alterar detalhes de um empregado";
-            public String op_7 = "Rodar a folha de pagamento para HOJE";
-            public String op_8 = "Desfazer ultima transação feita";
-            public String op_9 = "Refazer ultima transação desfeita";
-            public String op_10 = "Agendar um pagamento";
-            public String op_11 = "Criar nova agenda de pagamentos";
+            public String[] op = { "Adicionar um empregado", "Remover um empregado", "Lançar um cartão de ponto",
+                    "Lançar um resultado de venda", "Lançar uma taxa de serviço", "Alterar detalhes de um empregado",
+                    "Rodar a folha de pagamento para HOJE", "Desfazer ultima transação feita",
+                    "Refazer ultima transação desfeita", "Agendar um pagamento", "Criar nova agenda de pagamentos" };
         }
         ;
 
@@ -25,17 +18,17 @@ public class Menu {
         System.out.println();
         System.out.println("-- Sistema de folha de pagamento --");
         System.out.println();
-        System.out.printf("[1]: %s\n", options.op_1);
-        System.out.printf("[2]: %s\n", options.op_2);
-        System.out.printf("[3]: %s\n", options.op_3);
-        System.out.printf("[4]: %s\n", options.op_4);
-        System.out.printf("[5]: %s\n", options.op_5);
-        System.out.printf("[6]: %s\n", options.op_6);
-        System.out.printf("[7]: %s\n", options.op_7);
-        System.out.printf("[8]: %s\n", options.op_8);
-        System.out.printf("[9]: %s\n", options.op_9);
-        System.out.printf("[10]: %s\n", options.op_10);
-        System.out.printf("[11]: %s\n\n", options.op_11);
+        System.out.printf("[1]: %s\n", options.op[0]);
+        System.out.printf("[2]: %s\n", options.op[1]);
+        System.out.printf("[3]: %s\n", options.op[2]);
+        System.out.printf("[4]: %s\n", options.op[3]);
+        System.out.printf("[5]: %s\n", options.op[4]);
+        System.out.printf("[6]: %s\n", options.op[5]);
+        System.out.printf("[7]: %s\n", options.op[6]);
+        System.out.printf("[8]: %s\n", options.op[7]);
+        System.out.printf("[9]: %s\n", options.op[8]);
+        System.out.printf("[10]: %s\n", options.op[9]);
+        System.out.printf("[11]: %s\n\n", options.op[10]);
 
         System.out.println("Escolha uma opção");
         option = input.nextInt();
@@ -45,9 +38,12 @@ public class Menu {
 
         if (option > 11 || option < 1) {
             System.out.printf("A opção que você escolheu não existe.");
+        } else {
+            System.out.printf("%s.", options.op[option - 1]);
         }
 
         System.out.println();
+
     }
 
 }
